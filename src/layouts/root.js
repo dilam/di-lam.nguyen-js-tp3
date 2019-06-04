845c42b93f562bd3b8e4bba938b9492f8f1d821f
Di Lam NGUYEN <di-lam.nguyen@epita.fr>
2019-06-05 00:32:15 +0200

    [RM] checkout root.js

--- src/layouts/root.js
+++ src/layouts/root.js
@@ -3,30 +3,10 @@ import { Provider } from "react-redux";
 import Messages from "./Messages";
 import "antd/dist/antd.css";
 
-class Root extends React.Component {
-  constructor(props) {
-    super(props);
-
-    this.state = {
-      messages: []
-    }
-  }
-
-  pushMessage(message) {
-    let messages = this.state.messages;
-    messages.push(message);
-    this.setState({ messages: messages })
-
-    console.log(messages);
-  }
-
-  render() {
-    return (
-      <Provider store={this.props.store}>
-        <Messages />
-      </Provider>
-    );
-  }
+export default function Root(props) {
+  return (
+    <Provider store={props.store}>
+      <Messages />
+    </Provider>
+  );
 }
-
-export default Root;

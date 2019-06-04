7eb2ec802b85ccc63ac6cb0679531406d8290c62
Di Lam NGUYEN <di-lam.nguyen@epita.fr>
2019-06-05 01:49:35 +0200

    [ADD] lvl 2

--- src/layouts/root.js
+++ src/layouts/root.js
@@ -1,12 +1,28 @@
 import React from "react";
 import { Provider } from "react-redux";
 import Messages from "./Messages";
+import LoginLayout from "./authentication/Login";
 import "antd/dist/antd.css";
+import { connect } from "react-redux";
 
-export default function Root(props) {
-  return (
-    <Provider store={props.store}>
-      <Messages />
-    </Provider>
-  );
+const mapStateToProps = ownProps => {
+  return {
+    ...ownProps
+  }
 }
+
+class Root extends React.Component {
+  render() {
+    return (
+      <Provider store={this.props.store}>
+        {this.props.authentication.isLoggedIn ? (
+          <Messages />
+        ) : (
+          <LoginLayout />
+        )}
+      </Provider>
+    )
+  }
+}
+
+export default connect(mapStateToProps)(Root)
